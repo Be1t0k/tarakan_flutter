79f52e87b8c9c9b051bb9089a747dd46cad5001f
Be1t0k <moshkov.p2018@gmail.com>
2024-01-29 23:15:19 +0300

    and another one

--- lib/firebase/loginOrRegisterPage.dart
+++ lib/firebase/loginOrRegisterPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:student_test_system/register_page.dart';
+import 'package:student_test_system/firebase/register_page.dart';
 
 import 'login_page.dart';
 
