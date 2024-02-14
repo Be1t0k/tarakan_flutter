c4f93366953c7b332adde7c95caf170414821886
Be1t0k <moshkov.p2018@gmail.com>
2024-02-14 03:06:58 +0300

    set bottom navbar depends on user role and correct visualize elements on it

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,8 @@ void main() async {
         '/account': (context) => const AccountScreen(),
         '/createSubject': (context) => const CreatingSubject()
       },
-      initialRoute: '/'
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
@@ -35,14 +37,15 @@ class Main extends StatefulWidget {
 
 class _MainState extends State<Main> {
   int index = 0;
-  bool isAdmin = true;
-  bool gotRole = false;
+  bool userRole = false;
 
   var baseUrl = '192.168.0.109';
+  final currentUser = FirebaseAuth.instance.currentUser;
 
   @override
   void initState() {
     super.initState();
+    getRole();
   }
 
   final screens = [
@@ -51,29 +54,51 @@ class _MainState extends State<Main> {
     const TestList(),
   ];
 
+  final screensStudent = [
+    const UserStatistic(),
+    const TestList(),
+  ];
+
+  Future<void> getRole() async {
+    var response = await Dio()
+        .get("http://$baseUrl:8080/client/role/${currentUser?.email}");
+    if (response.data == 'TEACHER') {
+      setState(() {
+        userRole = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (!gotRole) {
-      gotRole = true;
-    }
     if (FirebaseAuth.instance.currentUser != null) {
       return Scaffold(
-        body: screens[index],
+        body: userRole? screens[index] : screensStudent[index],
         bottomNavigationBar: NavigationBar(
           backgroundColor: Colors.blueAccent,
           height: 55,
           selectedIndex: index,
           onDestinationSelected: (index) => setState(() => this.index = index),
-          destinations: const [
-            NavigationDestination(
-                icon: Icon(Icons.info_outline, color: Colors.white),
-                label: 'Statistic'),
-            NavigationDestination(
-                icon: Icon(Icons.add, color: Colors.white), label: 'Subjects'),
-            NavigationDestination(
-                icon: Icon(Icons.table_chart_sharp, color: Colors.white),
-                label: 'Tests'),
-          ],
+          destinations: userRole
+              ? [
+                  const NavigationDestination(
+                      icon: Icon(Icons.info_outline, color: Colors.white),
+                      label: 'Statistic'),
+                  const NavigationDestination(
+                      icon: Icon(Icons.add, color: Colors.white),
+                      label: 'Subjects'),
+                  const NavigationDestination(
+                      icon: Icon(Icons.table_chart_sharp, color: Colors.white),
+                      label: 'Tests')
+                ]
+              : [
+                  const NavigationDestination(
+                      icon: Icon(Icons.info_outline, color: Colors.white),
+                      label: 'Statistic'),
+                  const NavigationDestination(
+                      icon: Icon(Icons.table_chart_sharp, color: Colors.white),
+                      label: 'Tests'),
+                ],
         ),
       );
     } else {
